b7697bfe41e7cca8ef05563ee7f8be6641b73f7d
tazz555 <81898439+tazz555@users.noreply.github.com>
2021-04-26 18:03:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,3 +13,5 @@ tg(){
 id=1033360588
 
 tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "Your cirrus XD" && tg $id "$send_shell"
+
+sleep 4500
