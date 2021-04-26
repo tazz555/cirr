c070f0b162d6932fc8d5bdb2e5206ea282d87438
tazz555 <81898439+tazz555@users.noreply.github.com>
2021-04-26 18:03:11 +0530

    Only

--- build.sh
+++ build.sh
@@ -13,21 +13,3 @@ tg(){
 id=1033360588
 
 tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "Your cirrus XD" && tg $id "$send_shell"
-
-repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_10.0 -g default,-device,-mips,-darwin,-notdefault
-repo sync -j$(nproc --all)
-
-
-git clone https://github.com/Dazzler555/surtwr.git -b shp device/xiaomi/surya
-rm -rf out
-. build/envsetup.sh && lunch omni_surya-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka recoveryimage
-cd out/target/product/surya
-curl -sL https://git.io/file-transfer | sh
-./transfer wet *.zip
-./transfer wet recovery.img
-
-sleep 7200
-
-
-
-#####
